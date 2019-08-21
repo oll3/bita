08fa8895d9ecd677a84e71225ea555f85a094334
Olle Sandberg <olle@b3rg.se>
2019-08-21 20:09:45 +0200

    Run clippy and fmt checks in travisci.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub mod archive;
 pub mod archive_reader;
 pub mod buzhash;
-pub mod error;
 pub mod chunk_dictionary;
 pub mod chunker;
 pub mod chunker_utils;
 pub mod compression;
+pub mod error;
 
 pub mod file_archive_backend;
 pub mod para_pipe;
